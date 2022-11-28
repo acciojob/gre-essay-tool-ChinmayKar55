e8bae827c5cc78e3ed1f16004c6ba9093f6c3679
Chinmay <chinmaykarjan9@gmail.com>
2022-11-28 20:27:39 +0530

    Solution

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+function myFunction() {
+    
+    var x = document.getElementById("evaluatedText").value;
+    var a = x.split(" ");
+   
+    document.getElementById("wordCount").innerHTML =  a.length;
+  }
